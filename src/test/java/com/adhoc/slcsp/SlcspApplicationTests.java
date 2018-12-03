cd9210548082994cea8d29a00891257d111838f5
alexkcode <abstract.type.ak@gmail.com>
2018-12-02 18:07:09 -0600

    first commit

---
+++ src/test/java/com/adhoc/slcsp/SlcspApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adhoc.slcsp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlcspApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
